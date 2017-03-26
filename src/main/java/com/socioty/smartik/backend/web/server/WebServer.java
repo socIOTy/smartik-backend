77b1c4c686b1dfe8742da6decae14c649a4a8752
Willian <willian.campos@gmail.com>
2017-03-25 20:05:03 -0400

    Move background code to backend-background project

--- src/main/java/com/socioty/smartik/backend/web/server/WebServer.java
+++ src/main/java/com/socioty/smartik/backend/web/server/WebServer.java
@@ -1,57 +1,15 @@
 package com.socioty.smartik.backend.web.server;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import com.google.common.collect.Iterables;
-import com.google.gson.Gson;
-import com.socioty.smartik.backend.background.ActionListener;
-import com.socioty.smartik.backend.model.Account;
-import com.socioty.smartik.backend.model.Floor;
-import com.socioty.smartik.backend.model.Room;
-import com.socioty.smartik.backend.repositories.AccountRepository;
-
-import cloud.artik.api.DevicesApi;
-import cloud.artik.api.MessagesApi;
-import cloud.artik.client.ApiClient;
-import cloud.artik.client.ApiException;
-import cloud.artik.client.Configuration;
-import cloud.artik.client.auth.OAuth;
-import cloud.artik.model.DeviceTokenEnvelope;
-import cloud.artik.model.NormalizedMessage;
-import cloud.artik.model.NormalizedMessagesEnvelope;
-
 @EnableAutoConfiguration
 @EnableMongoRepositories(basePackages = { "com.socioty.smartik.backend.repositories" })
 @ComponentScan(basePackages = { "com.socioty.smartik.backend.web.resource" })
 public class WebServer implements CommandLineRunner {
-	
-	public static class GenerateTokenPayload {
-		private String access_token;
-		@SuppressWarnings("unused")
-		private String token_type;
-		@SuppressWarnings("unused")
-		private long expires_in;
-	}
-
-	@Autowired
-	private AccountRepository repository;
 
 	public static void main(final String[] args) throws Exception {
 		SpringApplication.run(WebServer.class, args);
@@ -59,88 +17,15 @@ public class WebServer implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		test();
-	}
-	
-	private void test() {
-		final Iterable<Account> accounts = findAllAccounts();
-		setUpDeviceSimulators(accounts);
-	}
-	
-//	private void createTestAccounts() {
-//		repository.save(new Account("willian.campos@gmail.com",
-//				new DeviceMap(Lists.newArrayList(
-//						new Floor(Sets.newHashSet(
-//								new Room("Living room", Sets.newHashSet("aa251f8f408a4b09ace604553cfc9f2d")))),
-//						new Floor(Sets.newHashSet(new Room("Bedroom", Sets.newHashSet())))))));
-//	}
-	
-	private Iterable<Account> findAllAccounts() {
-		// fetch all accounts
-		System.out.println("Accounts found with findAll():");
-		System.out.println("-------------------------------");
-		final List<Account> result = repository.findAll();
-		for (Account account : result) {
-			System.out.println(account);
-		}
-		System.out.println();
-		return result; 
-	}
-	
-	private void setUpDeviceSimulators(final Iterable<Account> accounts) {
-		final String accessToken = generateToken();
-		final ApiClient apiClient = Configuration.getDefaultApiClient();
-        final OAuth artikcloud_oauth = (OAuth) apiClient.getAuthentication("artikcloud_oauth");
-        artikcloud_oauth.setAccessToken(accessToken);
-		final DevicesApi devicesApi = new DevicesApi(apiClient);
-		final MessagesApi messagesApi = new MessagesApi(apiClient);
-		
-		for (final Account account : accounts) {
-			for (final Floor floor : account.getDeviceMap().getFloors()) {
-				for (final Room room : floor.getRooms()) {
-					for (final String deviceId : room.getDeviceIds()) {
-						try {
-							final DeviceTokenEnvelope deviceTokenEnvelope = devicesApi.updateDeviceToken(deviceId);
-							final String deviceToken = deviceTokenEnvelope.getData().getAccessToken();
-							final String deviceTypeId = devicesApi.getDevice(deviceId).getData().getDtid();
-							final NormalizedMessagesEnvelope messagesEnvelope = messagesApi.getNormalizedMessages(null, deviceId, null, null, null, null, 1, 100l, System.currentTimeMillis(), "desc");
-							final List<NormalizedMessage> messages = messagesEnvelope.getData();
-							final Map<String,Object> initialState = !messages.isEmpty() ? Iterables.getOnlyElement(messages).getData() : null; 
-							new ActionListener<>(deviceId, deviceToken, deviceTypeId, initialState).listen();
-						} catch (ApiException e) {
-							e.printStackTrace();
-							throw new RuntimeException(e);
-						}
-						
-					}
-				}
-			}
-		}
 	}
 
-	private String generateToken() {
-		try {
-			final CloseableHttpClient httpclient = HttpClients.createDefault();
-			final HttpPost httpPost = new HttpPost("https://accounts.artik.cloud/token?grant_type=client_credentials");
-			httpPost.addHeader("Authorization", "Basic ZjY4YzAzNzk1MWNkNDIyNDgxZGU0OTFiNGExZTE1M2Y6OWJjNTM4YjBiNjFhNGQ3MjhjMDU2MDE5NzU2ZWQ3Nzc=");
-			final StringEntity input = new StringEntity("{\"grant_type\":\"client_credentials\"}");
-			input.setContentType("application/json");
-			httpPost.setEntity(input);
-			final CloseableHttpResponse response = httpclient.execute(httpPost);
-			try {
-			    final HttpEntity entity = response.getEntity();
-			    final GenerateTokenPayload payoad = new Gson().fromJson(new InputStreamReader(entity.getContent()), GenerateTokenPayload.class);
-			    EntityUtils.consume(entity);
-			    return payoad.access_token;
-			} finally {
-			    response.close();
-			}
-	
-		} catch (final IOException e) {
-			throw new RuntimeException(e);
-		}
-		
-	}
-	
-	
+	// private void createTestAccounts() {
+	// repository.save(new Account("willian.campos@gmail.com",
+	// new DeviceMap(Lists.newArrayList(
+	// new Floor(Sets.newHashSet(
+	// new Room("Living room",
+	// Sets.newHashSet("aa251f8f408a4b09ace604553cfc9f2d")))),
+	// new Floor(Sets.newHashSet(new Room("Bedroom", Sets.newHashSet())))))));
+	// }
+
 }
--- src/test/java/com/smartik/backend/FirehoseWebSocketTest.java
+++ src/test/java/com/smartik/backend/FirehoseWebSocketTest.java
@@ -1,263 +1,263 @@
-package com.smartik.backend;
-
-import static org.junit.Assert.assertEquals;
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import cloud.artik.model.Acknowledgement;
-import cloud.artik.model.ActionOut;
-import cloud.artik.model.MessageIn;
-import cloud.artik.model.MessageOut;
-import cloud.artik.model.RegisterMessage;
-import cloud.artik.model.WebSocketError;
-import cloud.artik.websocket.ArtikCloudWebSocketCallback;
-import cloud.artik.websocket.DeviceChannelWebSocket;
-import cloud.artik.websocket.FirehoseWebSocket;
-
-public class FirehoseWebSocketTest extends ArtikCloudApiTest {
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    @Test
-    public void testFirehose1() throws Exception {
-        String deviceId = getProperty("device3.id");
-        String userId = getProperty("user1.id");
-        String accessToken = getProperty("device3.token");
-        
-        final CountDownLatch messageLatch = new CountDownLatch(1000);
-        
-        FirehoseWebSocket firehoseWS = new FirehoseWebSocket(accessToken, deviceId, null, null, userId,
-                new ArtikCloudWebSocketCallback() {
-
-                    @Override
-                    public void onAck(Acknowledgement ack) {
-                        System.out.println("onAck: " + ack);
-                    }
-
-                    @Override
-                    public void onAction(ActionOut action) {
-                        System.out.println("onAction: " + action);
-                    }
-
-                    @Override
-                    public void onClose(int code, String reason, boolean remote) {
-                        System.out.printf("onClose: %d %s %s\n", code, reason,
-                                remote);
-                    }
-
-                    @Override
-                    public void onError(WebSocketError error) {
-                        System.err.println("onError: " + error);
-                    }
-
-                    @Override
-                    public void onMessage(MessageOut message) {
-                        //System.out.println("Firehose: onMessage: " + message);
-                    }
-
-                    @Override
-                    public void onOpen(int httpStatus, String httpStatusMessage) {
-                        System.out.printf("onOpen: %d %s\n", httpStatus,
-                                httpStatusMessage);
-                    }
-
-                    @Override
-                    public void onPing(long timestamp) {
-                        System.err.println("onPing: " + timestamp);
-                    }
-
-                });
-
-        firehoseWS.connect();
-        
-        messageLatch.await(100, TimeUnit.SECONDS);
-
-        firehoseWS.closeBlocking();
-    }
-
-    
-    @Test
-    public void testFirehose2() throws Exception {
-        String deviceId = getProperty("device3.id");
-        String userId = getProperty("user1.id");
-        String accessToken = getProperty("device3.token");
-        
-        final CountDownLatch registerLatch = new CountDownLatch(1);
-        final CountDownLatch messageLatch = new CountDownLatch(1000);
-        DeviceChannelWebSocket ws = new DeviceChannelWebSocket(true, new ArtikCloudWebSocketCallback() {
-
-                    @Override
-                    public void onAck(Acknowledgement ack) {
-                        System.out.println("onAck: " + ack);
-                        if (ack.getCid().equalsIgnoreCase("first")) {
-                            registerLatch.countDown();
-                        } 
-                    }
-
-                    @Override
-                    public void onAction(ActionOut action) {
-                        System.out.println("onAction: " + action);
-                    }
-
-                    @Override
-                    public void onClose(int code, String reason, boolean remote) {
-                        System.out.printf("onClose: %d %s %s\n", code, reason,
-                                remote);
-                    }
-
-                    @Override
-                    public void onError(WebSocketError error) {
-                        System.err.println("onError: " + error);
-                    }
-
-                    @Override
-                    public void onMessage(MessageOut message) {
-                        System.out.println("onMessage: " + message);
-                    }
-
-                    @Override
-                    public void onOpen(int httpStatus, String httpStatusMessage) {
-                        System.out.printf("onOpen: %d %s\n", httpStatus,
-                                httpStatusMessage);
-                    }
-
-                    @Override
-                    public void onPing(long timestamp) {
-                        System.out.println("onPing: " + timestamp);
-                    }
-
-                });
-
-        ws.connectBlocking();
-
-        RegisterMessage msg = new RegisterMessage();
-        msg.setAuthorization("bearer " + accessToken);
-        msg.setSdid(deviceId);
-        msg.setCid("first");
-
-        ws.registerChannel(msg);
-        assertEquals(Boolean.TRUE,
-                new Boolean(registerLatch.await(10, TimeUnit.SECONDS)));
-
-        // Connect Firehose
-        FirehoseWebSocket firehoseWS = new FirehoseWebSocket(accessToken, deviceId, null, null, userId,
-                new ArtikCloudWebSocketCallback() {
-
-                    @Override
-                    public void onAck(Acknowledgement ack) {
-                        System.out.println("onAck: " + ack);
-                    }
-
-                    @Override
-                    public void onAction(ActionOut action) {
-                        System.out.println("onAction: " + action);
-                    }
-
-                    @Override
-                    public void onClose(int code, String reason, boolean remote) {
-                        System.out.printf("onClose: %d %s %s\n", code, reason,
-                                remote);
-                    }
-
-                    @Override
-                    public void onError(WebSocketError error) {
-                        System.err.println("onError: " + error);
-                    }
-
-                    @Override
-                    public void onMessage(MessageOut message) {
-                        System.out.println("Firehose: onMessage: " + message);
-                        messageLatch.countDown();
-                    }
-
-                    @Override
-                    public void onOpen(int httpStatus, String httpStatusMessage) {
-                        System.out.printf("onOpen: %d %s\n", httpStatus,
-                                httpStatusMessage);
-                    }
-
-                    @Override
-                    public void onPing(long timestamp) {
-                        System.err.println("onPing: " + timestamp);
-                    }
-
-                });
-
-        firehoseWS.connect();
-        
-        for (int i = 0; i < 1; i++) {
-            MessageIn message = new MessageIn();
-            message.setSdid(deviceId);
-            message.setTs(new Long(System.currentTimeMillis()));
-            
-//            "fields": {
-//                "colorRGB": {
-//                    "b": {
-//                        "type": "Integer",
-//                        "unit": "",
-//                        "isCollection": false,
-//                        "description": "blue component",
-//                        "metadata": {},
-//                        "tags": null
-//                    },
-//                    "g": {
-//                        "type": "Integer",
-//                        "unit": "",
-//                        "isCollection": false,
-//                        "description": "green component",
-//                        "metadata": {},
-//                        "tags": null
-//                    },
-//                    "r": {
-//                        "type": "Integer",
-//                        "unit": "",
-//                        "isCollection": false,
-//                        "description": "red component",
-//                        "metadata": {},
-//                        "tags": null
+//package com.smartik.backend;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//import java.util.concurrent.CountDownLatch;
+//import java.util.concurrent.TimeUnit;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import cloud.artik.model.Acknowledgement;
+//import cloud.artik.model.ActionOut;
+//import cloud.artik.model.MessageIn;
+//import cloud.artik.model.MessageOut;
+//import cloud.artik.model.RegisterMessage;
+//import cloud.artik.model.WebSocketError;
+//import cloud.artik.websocket.ArtikCloudWebSocketCallback;
+//import cloud.artik.websocket.DeviceChannelWebSocket;
+//import cloud.artik.websocket.FirehoseWebSocket;
+//
+//public class FirehoseWebSocketTest extends ArtikCloudApiTest {
+//
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+//
+//    @Test
+//    public void testFirehose1() throws Exception {
+//        String deviceId = getProperty("device3.id");
+//        String userId = getProperty("user1.id");
+//        String accessToken = getProperty("device3.token");
+//        
+//        final CountDownLatch messageLatch = new CountDownLatch(1000);
+//        
+//        FirehoseWebSocket firehoseWS = new FirehoseWebSocket(accessToken, deviceId, null, null, userId,
+//                new ArtikCloudWebSocketCallback() {
+//
+//                    @Override
+//                    public void onAck(Acknowledgement ack) {
+//                        System.out.println("onAck: " + ack);
 //                    }
-//                },
-//                "intensity": {
-//                    "type": "Integer",
-//                    "unit": "",
-//                    "isCollection": false,
-//                    "description": "Changes the intensity of the hue light (value in percent)",
-//                    "metadata": {},
-//                    "tags": null
-//                },
-//                "state": {
-//                    "type": "String",
-//                    "unit": "",
-//                    "isCollection": false,
-//                    "description": "state",
-//                    "metadata": {},
-//                    "tags": null
-//                }
-            
-            message.getData().put("state", "on");
-            message.getData().put("intensity", 100);
-            message.setCid("second");
-
-            ws.sendMessage(message);
-        }
-        
-        messageLatch.await(100, TimeUnit.SECONDS);
-
-        firehoseWS.closeBlocking();
-        
-        ws.closeBlocking();
-    }
-    
-    
-}
+//
+//                    @Override
+//                    public void onAction(ActionOut action) {
+//                        System.out.println("onAction: " + action);
+//                    }
+//
+//                    @Override
+//                    public void onClose(int code, String reason, boolean remote) {
+//                        System.out.printf("onClose: %d %s %s\n", code, reason,
+//                                remote);
+//                    }
+//
+//                    @Override
+//                    public void onError(WebSocketError error) {
+//                        System.err.println("onError: " + error);
+//                    }
+//
+//                    @Override
+//                    public void onMessage(MessageOut message) {
+//                        //System.out.println("Firehose: onMessage: " + message);
+//                    }
+//
+//                    @Override
+//                    public void onOpen(int httpStatus, String httpStatusMessage) {
+//                        System.out.printf("onOpen: %d %s\n", httpStatus,
+//                                httpStatusMessage);
+//                    }
+//
+//                    @Override
+//                    public void onPing(long timestamp) {
+//                        System.err.println("onPing: " + timestamp);
+//                    }
+//
+//                });
+//
+//        firehoseWS.connect();
+//        
+//        messageLatch.await(100, TimeUnit.SECONDS);
+//
+//        firehoseWS.closeBlocking();
+//    }
+//
+//    
+//    @Test
+//    public void testFirehose2() throws Exception {
+//        String deviceId = getProperty("device3.id");
+//        String userId = getProperty("user1.id");
+//        String accessToken = getProperty("device3.token");
+//        
+//        final CountDownLatch registerLatch = new CountDownLatch(1);
+//        final CountDownLatch messageLatch = new CountDownLatch(1000);
+//        DeviceChannelWebSocket ws = new DeviceChannelWebSocket(true, new ArtikCloudWebSocketCallback() {
+//
+//                    @Override
+//                    public void onAck(Acknowledgement ack) {
+//                        System.out.println("onAck: " + ack);
+//                        if (ack.getCid().equalsIgnoreCase("first")) {
+//                            registerLatch.countDown();
+//                        } 
+//                    }
+//
+//                    @Override
+//                    public void onAction(ActionOut action) {
+//                        System.out.println("onAction: " + action);
+//                    }
+//
+//                    @Override
+//                    public void onClose(int code, String reason, boolean remote) {
+//                        System.out.printf("onClose: %d %s %s\n", code, reason,
+//                                remote);
+//                    }
+//
+//                    @Override
+//                    public void onError(WebSocketError error) {
+//                        System.err.println("onError: " + error);
+//                    }
+//
+//                    @Override
+//                    public void onMessage(MessageOut message) {
+//                        System.out.println("onMessage: " + message);
+//                    }
+//
+//                    @Override
+//                    public void onOpen(int httpStatus, String httpStatusMessage) {
+//                        System.out.printf("onOpen: %d %s\n", httpStatus,
+//                                httpStatusMessage);
+//                    }
+//
+//                    @Override
+//                    public void onPing(long timestamp) {
+//                        System.out.println("onPing: " + timestamp);
+//                    }
+//
+//                });
+//
+//        ws.connectBlocking();
+//
+//        RegisterMessage msg = new RegisterMessage();
+//        msg.setAuthorization("bearer " + accessToken);
+//        msg.setSdid(deviceId);
+//        msg.setCid("first");
+//
+//        ws.registerChannel(msg);
+//        assertEquals(Boolean.TRUE,
+//                new Boolean(registerLatch.await(10, TimeUnit.SECONDS)));
+//
+//        // Connect Firehose
+//        FirehoseWebSocket firehoseWS = new FirehoseWebSocket(accessToken, deviceId, null, null, userId,
+//                new ArtikCloudWebSocketCallback() {
+//
+//                    @Override
+//                    public void onAck(Acknowledgement ack) {
+//                        System.out.println("onAck: " + ack);
+//                    }
+//
+//                    @Override
+//                    public void onAction(ActionOut action) {
+//                        System.out.println("onAction: " + action);
+//                    }
+//
+//                    @Override
+//                    public void onClose(int code, String reason, boolean remote) {
+//                        System.out.printf("onClose: %d %s %s\n", code, reason,
+//                                remote);
+//                    }
+//
+//                    @Override
+//                    public void onError(WebSocketError error) {
+//                        System.err.println("onError: " + error);
+//                    }
+//
+//                    @Override
+//                    public void onMessage(MessageOut message) {
+//                        System.out.println("Firehose: onMessage: " + message);
+//                        messageLatch.countDown();
+//                    }
+//
+//                    @Override
+//                    public void onOpen(int httpStatus, String httpStatusMessage) {
+//                        System.out.printf("onOpen: %d %s\n", httpStatus,
+//                                httpStatusMessage);
+//                    }
+//
+//                    @Override
+//                    public void onPing(long timestamp) {
+//                        System.err.println("onPing: " + timestamp);
+//                    }
+//
+//                });
+//
+//        firehoseWS.connect();
+//        
+//        for (int i = 0; i < 1; i++) {
+//            MessageIn message = new MessageIn();
+//            message.setSdid(deviceId);
+//            message.setTs(new Long(System.currentTimeMillis()));
+//            
+////            "fields": {
+////                "colorRGB": {
+////                    "b": {
+////                        "type": "Integer",
+////                        "unit": "",
+////                        "isCollection": false,
+////                        "description": "blue component",
+////                        "metadata": {},
+////                        "tags": null
+////                    },
+////                    "g": {
+////                        "type": "Integer",
+////                        "unit": "",
+////                        "isCollection": false,
+////                        "description": "green component",
+////                        "metadata": {},
+////                        "tags": null
+////                    },
+////                    "r": {
+////                        "type": "Integer",
+////                        "unit": "",
+////                        "isCollection": false,
+////                        "description": "red component",
+////                        "metadata": {},
+////                        "tags": null
+////                    }
+////                },
+////                "intensity": {
+////                    "type": "Integer",
+////                    "unit": "",
+////                    "isCollection": false,
+////                    "description": "Changes the intensity of the hue light (value in percent)",
+////                    "metadata": {},
+////                    "tags": null
+////                },
+////                "state": {
+////                    "type": "String",
+////                    "unit": "",
+////                    "isCollection": false,
+////                    "description": "state",
+////                    "metadata": {},
+////                    "tags": null
+////                }
+//            
+//            message.getData().put("state", "on");
+//            message.getData().put("intensity", 100);
+//            message.setCid("second");
+//
+//            ws.sendMessage(message);
+//        }
+//        
+//        messageLatch.await(100, TimeUnit.SECONDS);
+//
+//        firehoseWS.closeBlocking();
+//        
+//        ws.closeBlocking();
+//    }
+//    
+//    
+//}
